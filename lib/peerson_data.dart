cd3f23a215896c21c7e325de7000415ebf9b08f9
nikhilBangarwa <nikhilbangarwa476@gmail.com>
2024-11-17 10:17:58 +0530

    Delete lib directory

--- lib/peerson_data.dart
+++
@@ -1,8 +0,0 @@
-class Person{
-  String name;
-  String title;
-  String  image;
-
-
-  Person({required this.title,required this.name,required this.image});
-}
--- lib/whatshap_scrren/address_screen.dart
+++
@@ -1,8 +0,0 @@
-class Person{
-  String mes;
-  String time;
-  bool issend;
-
-
-  Person({required this.time,required this.mes,this.issend=false});
-}
